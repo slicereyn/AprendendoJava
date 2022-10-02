e38d8510ba92a479408c175a8fc7bc878b04e239
Izabelle Marques <85765308+slicereyn@users.noreply.github.com>
2022-10-02 18:05:06 -0300

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld{ //classes públicas, 1 classe por arquivo
-    public static void main(String args[]){ //main = módulo principal, que também é uma funcionalidade, por isso com (), precisa ser público para ser visto pela máquina virtual, e único (static), void = "não retorna informação", String = main pode receber parâmetros do conjunto args em texto
-        System.out.println("Oi, meu primeiro programa JAVA!"); //O System gerencia o sistema, o out é saída padrão (tela), o print imprime
-    }
-}
-
-//jdk = java developer kit
-//jre = java runtime enviroment
