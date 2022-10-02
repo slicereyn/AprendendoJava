ad9382a8367fa622852db7701d0a61459e70410a
Izabelle Marques <85765308+slicereyn@users.noreply.github.com>
2022-10-02 18:04:51 -0300

    Create HelloWorld.java

---
+++ Aula1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{ //classes públicas, 1 classe por arquivo
+    public static void main(String args[]){ //main = módulo principal, que também é uma funcionalidade, por isso com (), precisa ser público para ser visto pela máquina virtual, e único (static), void = "não retorna informação", String = main pode receber parâmetros do conjunto args em texto
+        System.out.println("Oi, meu primeiro programa JAVA!"); //O System gerencia o sistema, o out é saída padrão (tela), o print imprime
+    }
+}
+
+//jdk = java developer kit
+//jre = java runtime enviroment
